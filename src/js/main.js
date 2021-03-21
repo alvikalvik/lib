8a84751e94091488158f5470bfa7dfb6eb5a40e1
alvikalvik <alvikalvik@gmail.com>
2021-03-21 16:01:14 +0200

    dropdown menu component done

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import './lib/lib';
+import $ from './lib/lib';
 
 $('#first').on('click', () => {
     $('div').eq(1).fadeToggle(800);
@@ -11,3 +12,16 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(`
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+            <a href="#" class="dropdown-item">Action #1</a>
+            <a href="#" class="dropdown-item">Action #2</a>
+            <a href="#" class="dropdown-item">Action #3</a>
+        </div>
+    </div>
+`);
+
+$('.dropdown-toggle').dropdown();
